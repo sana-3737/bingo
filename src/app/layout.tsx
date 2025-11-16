32d9a3315eff33f41bedb4b28c3d5a758f716544
sana-3737 <sana.fuwaru@gmail.com>
2025-11-16 21:05:43 +0900

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { BingoProvider } from "@/context/bingoContext";
+import { BingoProvider } from '../context/bingoContext';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
