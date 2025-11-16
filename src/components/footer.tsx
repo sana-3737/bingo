1bbbaed0eb1c1459473852809c1f673a5880265a
sana-3737 <sana.fuwaru@gmail.com>
2025-11-16 17:30:00 +0900

    make

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center p-4 mt-6">
+      <p className="text-sm text-gray-600">© 2025 Bingo Event App</p>
+    </footer>
+  );
+}
